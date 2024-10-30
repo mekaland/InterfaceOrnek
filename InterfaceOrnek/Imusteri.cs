41c9b4f1861a56a005566c6cc52123441972f137
Kadir Dagli <mehmetkadirdagli27@gmail.com>
2024-10-30 21:16:53 +0300

    Add project files.

---
+++ InterfaceOrnek/Imusteri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceOrnek
+{
+	public interface Imusteri
+	{
+		//abstarct ile interface arasıdaki fark interface in tamamen şablon yapısına sahip olmasıdır
+		//abstarctda nesne örneklenemiyor ama içeride method oluşturabiliyorum.iNTERFACe içerisinde yapıcı metot kullanılmaz ve nesne örneği alınmaz
+		//abstarct da kalıtım yoluyla nesne örneği alınabilir
+		// interface de hiçbir methodun bodysi bulunmaz 
+
+		int Id { get; set; }
+		string Name { get; set; }
+		string Soyisim { get; set; }
+
+		//metot
+		int yeniKayit(string isim, string soyisim);
+		int kayitDuzenle(int id ,string soyisim , string isim);
+
+		int kayitSil(int id);
+	}
+}
---
+++ InterfaceOrnek/musteri.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceOrnek
+{
+	public class musteri : Imusteri
+	{
+		int _id;
+		string _name;
+		string _soyisim;
+		public int Id { get { return _id; }  set { _id = value; }}
+		public string Name { get { return _name; } set { _name	 = value; } }
+		public string Soyisim { get { return _soyisim; } set { _soyisim = value; } }
+
+		public int kayitDuzenle(int id, string soyisim, string isim)
+		{
+			Console.WriteLine("kayıt düzenlendi");
+			return 1;
+		}
+
+		public int kayitSil(int id)
+		{
+			Console.WriteLine("kayıt silindi");
+			return 1;
+		}
+
+		public int yeniKayit(string isim, string soyisim)
+		{
+			Console.WriteLine(" yenikayıt eklendi");
+			return 1;
+		}
+	}
+}
